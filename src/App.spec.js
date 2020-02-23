7e65601210ac9f88564479584b405d386a93c754
EmyRukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-3.local>
2020-02-23 02:48:42 +0200

    fixing error

---
+++ src/App.spec.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import axios from 'axios';
+import App, { Counter, dataReducer } from './App';
+const list = ['a', 'b', 'c'];
+describe('App', () => {
+ 
+  test('snapshot renders', () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('renders the inner Counter', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Counter).length).toEqual(1);
+  });
+  it('passes all props to Counter', () => {
+    const wrapper = mount(<App />);
+    const counterWrapper = wrapper.find(Counter);
+    expect(counterWrapper.find('p').text()).toEqual('0');
+  });
+
+  it('passes all props to Counter', () => {
+    const wrapper = mount(<App />);
+    const counterWrapper = wrapper.find(Counter);
+    expect(counterWrapper.find('p').text()).toEqual('0');
+  });
+  it('increments the counter', () => {
+    const wrapper = mount(<App />);
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click');
+    const counterWrapper = wrapper.find(Counter);
+    expect(counterWrapper.find('p').text()).toBe('1');
+  });
+  it('decrements the counter', () => {
+    const wrapper = mount(<App />);
+    wrapper
+      .find('button')
+      .at(1)
+      .simulate('click');
+    const counterWrapper = wrapper.find(Counter);
+    expect(counterWrapper.find('p').text()).toBe('-1');
+  });
+});
+describe('Reducer', () => {
+  it('fetches async data', () => {
+    axios.get = jest.fn(() => promise);
+      setTimeout(
+        () =>
+          resolve({
+            data: {
+              hits: [
+                { objectID: '1', title: 'a' },
+                { objectID: '2', title: 'b' },
+              ],
+            },
+          }),
+        100
+      )
+    const wrapper = mount(<App />);
+    expect(wrapper.find('li').length).toEqual(0);
+    promise.then(() => {
+      expect(wrapper.find('li').length).toEqual(2);
+      axios.get.mockClear();
+    });
+  });
+});
